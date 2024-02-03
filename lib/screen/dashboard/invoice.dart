c552b4c24f03c44b14d20bcabe7d2e4a22b6de67
Dell <29priyanka97@gmail.com>
2024-02-03 17:49:49 +0530

    invoice

---
+++ lib/screen/dashboard/invoice.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Invoice extends StatefulWidget {
+  const Invoice({super.key});
+
+  @override
+  State<Invoice> createState() => _InvoiceState();
+}
+
+class _InvoiceState extends State<Invoice> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
